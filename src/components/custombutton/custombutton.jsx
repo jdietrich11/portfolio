0d781089eebde762bb8b660a7fff67840fc620e3
jdietrich11 <jbdietrich2@gmail.com>
2022-02-27 04:48:50 -0700

    quick fix button styling

--- src/components/custombutton/custombutton.jsx
+++ src/components/custombutton/custombutton.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import './custombutton.styles.scss';
 
 const CustomButton = (data) => {
-  console.log(data);
   return (
     <>
       <button className='button'>{data.data}</button>
