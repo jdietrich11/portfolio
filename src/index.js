f41fc4c9d94310ae41f305ed4c5043b3675f3f99
jdietrich11 <jbdietrich2@gmail.com>
2022-03-11 01:08:45 -0700

    added some resume info

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import './firebase/config';
+
 import App from './pages/app/app';
 import AboutMe from './pages/aboutme/aboutme';
 import Resume from './pages/resume/resume';
--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import Footer from '../../components/footer/footer';
+
 class Resume extends React.Component {
   render() {
     return (
       <>
-        <div>Resume test</div>
+        <div>
+          <div>Joshua Dietrich</div>
+          <div>Web Developer</div>
+          <div>
+            <div>3069 West 14600 South, Bluffdale, Utah, 84065</div>
+            <div>jbdietrich2@gmail.com</div>
+            <div>(385) 424-2323</div>
+          </div>
+          <Footer />
+        </div>
       </>
     );
   }
