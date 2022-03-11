6ca676ae045c524e13c95b301649ae79d6b0a48e
jdietrich11 <jbdietrich2@gmail.com>
2022-03-11 04:01:41 -0700

    updating styling and content in the personal info page

--- src/components/about/outdoors/outdoors.jsx
+++ src/components/about/outdoors/outdoors.jsx
@@ -6,7 +6,36 @@ const Outdoors = () => {
       <div className='sidename'>
         <div className='sidename-title'>Outdoors</div>
       </div>
-      <div className='content'></div>
+      <div className='content'>
+        <div className='question'>What Outdoor activities do you enjoy?</div>
+        <div className='answer'>
+          <ul>
+            <li>sports</li>
+            <li>camping</li>
+            <li>motorcyle rides</li>
+            <li>drives</li>
+            <li>4-wheeling</li>
+            <li>rock climbing</li>
+            <li>Mountain Biking</li>
+          </ul>
+        </div>
+        <div className='question'>What type of Motorcycling?</div>
+        <div className='answer'>
+          Both riding dirt bikes off road and riding a road bike. I own a Yamaha
+          R3 and enjoy a weekend ride up a canyon or ride around Utah lake,
+          given an opportunity I would not pass up on a chance to go ride dirt
+          bikes either.
+        </div>
+        <div className='question'>Do you have a favorite camping spot?</div>
+        <div className='answer'>
+          Yeah, growing up my family would go on a yearly camping trip over the
+          summer with my extended family. We would usually go to a place called
+          Potters Ponds. We would go on 4-Wheeler rides, my favorite is trail
+          #5. We would also have Airsoft wars in the forested areas, and always
+          do a friday night movie, using a projector we would watch a movie
+          while it was dark by the fire.
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/about/sports/sports.jsx
+++ src/components/about/sports/sports.jsx
@@ -6,7 +6,35 @@ const Sports = () => {
       <div className='sidename'>
         <div className='sidename-title'>Sports</div>
       </div>
-      <div className='content'></div>
+      <div className='content'>
+        <div className='question'>What sports?</div>
+        <div className='answer'>
+          <ul>
+            <li>Basketball</li>
+            <li>Football</li>
+            <li>Golf</li>
+          </ul>
+        </div>
+        <div className='question'>
+          Did you ever play any sports competitively?
+        </div>
+        <div className='answer'>
+          Aside from intramural Basketball not in college. In High School I was
+          a QB on the football team until I tore my ACL. I tried out for the
+          Basktball team but did not make it? Until recently, my Dad and I would
+          enter the Bluffdale City golf tournament each year.
+        </div>
+        <div className='question'>Do you play any other sports?</div>
+        <div className='answer'>
+          I don't get alot of opportunites to play many other sports but I'm
+          always up for it.
+        </div>
+        <div className='question'>Are there any sports you do NOT like?</div>
+        <div className='answer'>
+          Baseball, though it has nothing to do with the sport itself and has
+          more to do with the people who I grew up with who did play baseball.
+        </div>
+      </div>
     </div>
   );
 };
